8ec4e328c9e0baaeb2808ef209ddf56cd00726b1
Robert Admin <robert.admin@eglgh.com>
2024-08-26 20:48:14 +0000

    product category

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import update_product, delete_product
 
 urlpatterns = [
     path('products', views.ProductsListCreate.as_view()),
+    path('product/<int:pk>',views.GetSinleProduct.as_view()),
+    path('product/<int:pk>/update', update_product),
+    path('product/<int:pk>/delete', delete_product),
     path('category', views.CategoryListCreate.as_view()),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, permissions
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -11,12 +14,57 @@ class ProductsListCreate(generics.ListCreateAPIView):
 
 
     def perform_create(self,serializer):
-        serializer.save(user=self.request.user)
+        serializer.save()
+
+class GetSinleProduct(generics.RetrieveAPIView): 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer 
+
+
+@api_view(['PATCH'])
+def update_product(request,pk):
+    try:
+       product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(product,data=request.data, partial=True)
+    
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_product(request,pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+       
 
 
 class CategoryListCreate(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self,serializer):
-        serializer.save()  
+        serializer.save(user=self.request.user)  
+
+
+@api_view(['DELETE'])
+def delete_category(request,pk):
+    try:
+        category = Category.objects.get(pk)
+    except Category.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    category.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)    
+
+
+
